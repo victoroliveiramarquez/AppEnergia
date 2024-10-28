c95b71737489aa610ea25cd9ed8145c62cb8cda7
victoroliveiramarquez <v13.oliveira.m@gmail.com>
2024-10-28 10:50:43 +0100

    Creacion de 4 json para el DetallesFragment y que aparezcan de manera Random despues de cada llamada

--- app/src/main/java/com/aplicacion2/appenergia/samartsolar/SmartSolarService.kt
+++ app/src/main/java/com/aplicacion2/appenergia/samartsolar/SmartSolarService.kt
@@ -1,12 +1,19 @@
 package com.aplicacion2.appenergia.samartsolar
 
 import co.infinum.retromock.meta.Mock
+import co.infinum.retromock.meta.MockRandom
 import co.infinum.retromock.meta.MockResponse
 import retrofit2.http.GET
 
 interface SmartSolarService {
+
     @Mock
+    @MockRandom
     @MockResponse(body = "detalles.json")
+    @MockResponse(body = "detalles2.json")
+    @MockResponse(body = "detalles3.json")
+    @MockResponse(body = "detalles4.json")
+    @MockResponse(body = "detalles5.json")
     @GET("/")
     suspend fun getSmartSolarDetails(): SmartSolarDetails
 }
